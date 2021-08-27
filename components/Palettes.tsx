b7356c2945f744ed8584253879aaece4be5bb75a
Rohit Bhurtel <rohitbhurtel@192-168-1-105.tpgi.com.au>
2021-08-27 11:26:56 +1000

    cloned the project from prev directory

---
+++ components/Palettes.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export interface PalettesProps {}
+
+export interface PalettesState {}
+
+class Palettes extends Component<PalettesProps, PalettesState> {
+  render() {
+    return <div>Hi I am Palettes component</div>;
+  }
+}
+
+export default Palettes;
