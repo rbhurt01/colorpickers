59938cb8557bc440188c017b5bca1c3778125847
Rohit Bhurtel <rohitbhurtel@192-168-1-105.tpgi.com.au>
2021-08-27 11:45:00 +1000

    check commit

--- components/Palettes.tsx
+++ components/Palettes.tsx
@@ -6,7 +6,7 @@ export interface PalettesState {}
 
 class Palettes extends Component<PalettesProps, PalettesState> {
   render() {
-    return <div>Hi I am Palettes component</div>;
+    return <div>checking the Palettes</div>;
   }
 }
 
